54951c9f0dc95d4a7c8cf3899dd9cb18070f6fa0
ewand <ewand@EWANS_LAPTOP>
2025-11-11 19:11:32 -0500

    chicken wings :D

---
+++ branch-exercise/SoundFX.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MohawkGame2D
+{
+    internal class SoundFX
+    {
+        // code code code
+        // i love doing my job
+
+        float numberOfChickenWings = 37;
+
+        public void Setup()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+
+}
